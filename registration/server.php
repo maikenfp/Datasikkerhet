543afe004f7bd4f1f450d279fb52f895a6a35a8a
Andreas <thaulandandy@gmail.com>
2022-01-22 16:45:45 +0100

    Update server.php

---
+++ registration/server.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$username = "";
+$email = "";
+$passord = "";
+$errors = array();
+
+// koble til database
+include_once '../../config/Database.php';
+
+$database = new Database();
+$db = $database->connect();
+
+// Registere bruker
+if(isset($_POST["reg_user"])) {
+
+}
