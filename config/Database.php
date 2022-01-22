4f64551618c82c58e9ba0eb612542e2a2ca5ab23
Andreas <thaulandandy@gmail.com>
2022-01-22 16:54:33 +0100

    added database

--- config/Database.php
+++ config/Database.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
   class Database {
     // DB Params
     private $host = 'localhost';
@@ -11,7 +11,7 @@
     public function connect() {
       $this->conn = null;
 
-      try {
+      try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
