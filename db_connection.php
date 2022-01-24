6502a6011ed1f7f99bbbccdfb00ffa25bb5351b4
Børre Stellander <stellander88@gmail.com>
2022-01-24 22:30:15 +0100

    test db

--- db_connection.php
+++ db_connection.php
@@ -2,9 +2,9 @@
 function OpenCon()
  { 
  $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "example";
+ $dbuser = "test";
+ $dbpass = "test123";
+ $db = "emne";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
