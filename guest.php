2ecc505c29ed2b5b187e378d63a5143dc4efaeca
Børre Stellander <stellander88@gmail.com>
2022-01-25 14:25:54 +0100

    Changed db connection

--- guest.php
+++ guest.php
@@ -2,13 +2,13 @@
 <html lang="NB-NO">
     <head>
         <meta charset="utf-8">
-        <title>Hiof tilbakemelding | Gjestebruker</title>
+        <title>Emne tilbakemelding | Gjestebruker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- dimensions and scaling for the browser -->
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
         <header>
-            <h1>Tilbakemeldinger for fag på Høgskolen i Østfold </h1>
+            <h1>Tilbakemeldinger for emner</h1>
         </header>
         <main>
             <form action="" method="post">
@@ -18,28 +18,31 @@
             </form>
 
             <?php
-                $dbhost = "localhost";
-                $dbuser = "test";
-                $dbpass = "test123";
-                $db = "datasikkerhet";
-                $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-           
+                include_once "config/Database.php";
+
+                $database = new Database();
+                $db = $database->connect();
+
                 $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
                     melding.svar, melding.spørsmål
                     FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id where pinkode = $_POST[pin]";
-                $result = $conn->query($sql);  
 
-                if ($result->num_rows > 0){
-                    $row = $result->fetch_assoc();
-                    echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
+                $stmt = $db->query($sql);
+                $result = $db->query($sql);
+                $row_count = $result->fetchColumn();
 
-                    //output all emne info from db
-                    while($row = $result->fetch_assoc()) {
+                $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($row){
+
+                    foreach ($row as $row) {
+                        echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
                         echo "<br><br>Spørsmål: " . $row["spørsmål"]. "<br> Svar: ". $row["svar"]. "<br>";
                         ?>
+
                         <!-- Submit comment -->
                         <form action="" method="post">
-                            <input type="text" name="Kommenter">
+                            <textarea name="Kommenter" rows="4" cols="28">
+                            </textarea>
                             <button type="submit">Kommenter</button>
                         </form>
                         <?php
@@ -51,9 +54,6 @@
                     echo "Ingen resultat";
                 }
 
-
-
-                $conn->close();
             ?>
         </main>
     </body>
