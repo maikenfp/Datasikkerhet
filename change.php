d74f8892a2b8ac9d8344f9114603f632dbf6fbe7
Maiken Persen <54583559+maikenfp@users.noreply.github.com>
2022-01-27 17:19:25 +0100

    nesten ferdig med bytting av passord

---
+++ change.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body> 
+        <form action='switch.php' method="POST">
+            <input type='text' name="epost">
+            <input type='text' name="passord">
+            <input type='text' name="nyttpassord">
+            <input type="submit" name="submit_email">
+        </form>
+    </body>
+</html>
