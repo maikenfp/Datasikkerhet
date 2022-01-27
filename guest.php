f6b9b49583856d38753a959be72264b37df0ad43
Børre Stellander <stellander88@gmail.com>
2022-01-27 14:51:48 +0100

    Added comment on message function

--- guest.php
+++ guest.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Emne tilbakemelding | Gjestebruker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- dimensions and scaling for the browser -->
-        <link rel="stylesheet" href="styles.css">
+        <!-- <link rel="stylesheet" href="style.css"> -->
     </head>
     <body>
         <header>
@@ -13,40 +13,52 @@
         <main>
             <form action="" method="post">
                 <label for="pinkode">Pin-kode:</label>
-                <input type="number" name="pin" min="0" max="9999" >
+                <input type="number" name='pin' min="0" max="9999" >
                 <button type="submit">Søk</button>    
             </form>
+
 
             <?php
                 include_once "config/Database.php";
 
+                $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
+                    melding.svar, melding.spørsmål, melding.melding_id
+                    FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id WHERE pinkode=$_POST[pin]";
+
                 $database = new Database();
                 $db = $database->connect();
-
-                $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
-                    melding.svar, melding.spørsmål
-                    FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id where pinkode = $_POST[pin]";
-
                 $stmt = $db->query($sql);
-                $result = $db->query($sql);
-                $row_count = $result->fetchColumn();
-
+                // $result = $db->query($sql);
+                // $row_count = $result->fetchColumn();
                 $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                if($row){
 
+
+                if($row){
+    
                     foreach ($row as $row) {
                         echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
-                        echo "<br><br>Spørsmål: " . $row["spørsmål"]. "<br> Svar: ". $row["svar"]. "<br>";
+                        echo "<br><br>Spørsmål: " . $row["spørsmål"]. "<br> Svar: ". $row["svar"]. "<br>" 
+                        ."<br> Melding_id:". $row["melding_id"];
                         ?>
 
                         <!-- Submit comment -->
-                        <form action="" method="post">
-                            <textarea name="Kommenter" rows="4" cols="28">
-                            </textarea>
-                            <button type="submit">Kommenter</button>
-                        </form>
                         <?php
-                        
+                        if (isset($_POST["button"])){
+                            commentMessage($_POST["kommenter"], $_POST["meldingID"]);
+                            // commentMessage($_POST["kommenter"], "$row[melding_id]");
+                        }
+                        ?>
+
+                        <form method="POST">
+                                    <label>Melding</label>
+                                    <textarea name="kommenter" >
+                                    </textarea>
+                                    <label>Melding_id</label>
+                                    <input type="number" name="meldingID">
+                                    <button type="submit" name="button"> kommenter</button>
+                                </form>
+
+                        <?php
                     }
                 } 
                 
@@ -59,6 +71,21 @@
     </body>
 </html>
 
+<?php
+
+function commentMessage($kommentar, $melding_id){
+    include_once "config/Database.php";
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $sql = "INSERT INTO kommentar (kommentar, melding_id) 
+            VALUES ('$kommentar', '$melding_id')";
+    $stmt = $db->query($sql);
+}
+
+
+?>
 
 
 
