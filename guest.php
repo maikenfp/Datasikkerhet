7ee957fd9cba24d13beb3295a8097e90677254d1
Børre Stellander <stellander88@gmail.com>
2022-01-27 16:18:25 +0100

    Comment on message

--- guest.php
+++ guest.php
@@ -11,70 +11,73 @@
             <h1>Tilbakemeldinger for emner</h1>
         </header>
         <main>
-            <form action="" method="post">
-                <label for="pinkode">Pin-kode:</label>
-                <input type="number" name='pin' min="0" max="9999" >
-                <button type="submit">Søk</button>    
+            <form method="post">
+                <label>Pin-kode:</label>
+                <input type="number" name='pinkode' min="0" max="9999" >
+                <button type="submit" name="PinButton">Søk</button>    
+            </form>
+            <?php
+                if (isset($_POST["PinButton"])){
+                   getMessage($_POST["pinkode"]);
+                }
+            
+             
+                if (isset($_POST["button"])){
+                    commentMessage($_POST["kommenter"], $_POST["meldingID"]);
+                    // commentMessage($_POST["kommenter"], "$row[melding_id]");
+                }
+            ?>
+
+            <form method="POST">
+                    <br>
+                    <label>Kommentar:</label>
+                    <br>
+                    <textarea name="kommenter" >
+                    </textarea>
+                    <br>
+                    <label>Melding_id:</label>
+                    <br>
+                    <input type="number" name="meldingID">
+                    <button type="submit" name="button"> kommenter</button>
             </form>
 
-
-            <?php
-                include_once "config/Database.php";
-
-                $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
-                    melding.svar, melding.spørsmål, melding.melding_id
-                    FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id WHERE pinkode=$_POST[pin]";
-
-                $database = new Database();
-                $db = $database->connect();
-                $stmt = $db->query($sql);
-                // $result = $db->query($sql);
-                // $row_count = $result->fetchColumn();
-                $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-                if($row){
-    
-                    foreach ($row as $row) {
-                        echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
-                        echo "<br><br>Spørsmål: " . $row["spørsmål"]. "<br> Svar: ". $row["svar"]. "<br>" 
-                        ."<br> Melding_id:". $row["melding_id"];
-                        ?>
-
-                        <!-- Submit comment -->
-                        <?php
-                        if (isset($_POST["button"])){
-                            commentMessage($_POST["kommenter"], $_POST["meldingID"]);
-                            // commentMessage($_POST["kommenter"], "$row[melding_id]");
-                        }
-                        ?>
-
-                        <form method="POST">
-                                    <label>Melding</label>
-                                    <textarea name="kommenter" >
-                                    </textarea>
-                                    <label>Melding_id</label>
-                                    <input type="number" name="meldingID">
-                                    <button type="submit" name="button"> kommenter</button>
-                                </form>
-
-                        <?php
-                    }
-                } 
-                
-                else {
-                    echo "Ingen resultat";
-                }
-
-            ?>
         </main>
     </body>
 </html>
 
 <?php
 
+function getMessage($pin){
+    include_once "config/Database.php";
+    $database = new Database();
+    $db = $database->connect();
+
+    
+    $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
+        melding.svar, melding.spørsmål, melding.melding_id
+        FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id WHERE pinkode='$pin'";
+
+    
+    $stmt = $db->query($sql);
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+
+    if($row){
+        
+        foreach ($row as $row) {
+            echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
+            echo "<br><br> Melding_id:". $row["melding_id"]."<br> Spørsmål: ". $row["spørsmål"]. "<br> Svar: ". $row["svar"]. "<br>";
+        }
+    }
+    else {
+        echo "Ingen resultat";
+    }
+    
+}
+
 function commentMessage($kommentar, $melding_id){
     include_once "config/Database.php";
+    echo "comment";
 
     $database = new Database();
     $db = $database->connect();
