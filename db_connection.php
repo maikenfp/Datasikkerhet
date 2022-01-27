cce654a3cf4f7c1f38c8f41c5c05a877d8e88149
hstangborli <haakon.stangborli@gmail.com>
2022-01-27 16:06:02 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "example";
+ $db = "datasikkerhet";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
