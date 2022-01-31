f00185d038f8192f809e30eb5d1f4064789ef9dd
Maiken Persen <54583559+maikenfp@users.noreply.github.com>
2022-01-31 10:50:25 +0100

    "oppgradert" bytting av passord

--- change.php
+++ change.php
@@ -4,9 +4,19 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body> 
+
+    <h1>Bytt passord</h1>
         <form action='switch.php' method="POST">
+
+        <?php if (isset($_GET['error'])) { ?>
+	            <p class="error"><?php echo $_GET['error']; ?></p>
+	    <?php } ?>
+
+        <label>E-post</label>
             <input type='text' name="epost">
+        <label>Passord</label>
             <input type='text' name="passord">
+        <label>Nytt passord</label>
             <input type='text' name="nyttpassord">
             <input type="submit" name="submit_email">
         </form>
