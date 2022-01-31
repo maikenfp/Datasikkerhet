90817f4023b46882c7b8ce5db52072caed09a448
Andreas <thaulandandy@gmail.com>
2022-01-31 16:27:13 +0100

    Link til login
    La til en link til login siden fra registerings siden

--- registration/index.php
+++ registration/index.php
@@ -8,15 +8,17 @@
         <div class="header">
             <h2>Hva slags bruker vil du registere?</h2>
         </div>
- <form action="student.php" method="get">
-    <div>
-        <input type="submit" value="STUDENT">
-    </div>
-</form>
-<form action="foreleser.php" method="get">
-    <div>
-        <input type="submit" value="FORELESER">
-    </div>
-</form>
+        <form action="student.php" method="get">
+            <div>
+                <input type="submit" value="STUDENT">
+            </div>
+        </form>
+        <form action="foreleser.php" method="get">
+            <div>
+                <input type="submit" value="FORELESER">
+            </div>
+        </form>
+        <h1>HAR DU ALLEREDE EN BRUKER? LOGG INN HER:</h1>
+        <a href="../index.php">LOGG INN</a>
     </body>
 </html>
