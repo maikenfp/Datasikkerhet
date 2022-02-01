7165c30a8d0138d33eaf49224d6e92538f75f346
Maiken Persen <54583559+maikenfp@users.noreply.github.com>
2022-02-01 13:18:09 +0100

    picture

--- registration/registration_foreleser.php
+++ registration/registration_foreleser.php
@@ -6,10 +6,36 @@ $database = new Database();
 $db = $database->connect();
 
 if(isset($_POST["fore_reg"])) { // Requester action fra knappen som er til registering av studenter på index.php
+    $imgFile = $_FILES['picture']['name'];
+    $tmp_dir = $_FILES['picture']['tmp_name'];
+    $imgSize = $_FILES['picture']['size'];
+    if(!empty($imgFile)){
+
+        $upload_dir = '../photos/';
+    
+        $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); 
+    
+        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); 
+        $coverpic = rand(1000,1000000).".".$imgExt;
+    
+        if(in_array($imgExt, $valid_extensions)){
+            if($imgSize < 5000000){
+                move_uploaded_file($tmp_dir,$upload_dir.$coverpic);
+                echo "Opplasting ferdig";
+            }
+            else{
+                $errMSG = "Filen er for stor";
+            }
+        }
+        else{
+            $errMSG = "Kun JPG, JPEG, PNG & GIF filer tillatt";
+        }
+    }
     $username = strip_tags($_POST["navn"]);
     $email = strip_tags($_POST["epost"]);
     $password = strip_tags($_POST["passord"]);
     $course = strip_tags($_POST["studieretning"]);
+    
 
     if(empty($username)) {
         header("Location: index.php?error=Du må skrive inn navn!");
