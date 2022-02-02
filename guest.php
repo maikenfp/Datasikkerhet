d0da43c91ae1dcf5127ec29a97a03ce74c656a94
Joakim <skjelbred99@hotmail.no>
2022-02-02 16:30:30 +0100

    fikset error når man logger ut som guest

--- guest.php
+++ guest.php
@@ -4,100 +4,105 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="NB-NO">
-    <head>
-        <meta charset="utf-8">
-        <title>Emne tilbakemelding | Gjestebruker</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- dimensions and scaling for the browser -->
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <header>
-            <h1>Tilbakemeldinger for emner</h1>
-        </header>
-        <main>
-            <form method="post">
-                <label>Pin-kode:</label>
-                <input type="number" name='pinkode' min="0" max="9999" >
-                <button type="submit" name="PinButton">Søk</button>    
-            </form>
-            <a href="/logout.php">Logg ut</a>
-            <?php
-                // When guest user comment on message, sudent_id = NULL
-                $currentStudentId = 0; 
-                if(session_id() != ''){
-                    //session has started
-                    if(isset($_SESSION["student_id"])){
-                        $currentStudentId = $_SESSION["student_id"];
-                    }
-                }
 
-                echo "<br>";
+<head>
+    <meta charset="utf-8">
+    <title>Emne tilbakemelding | Gjestebruker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- dimensions and scaling for the browser -->
+    <link rel="stylesheet" href="style.css">
+</head>
 
-                include_once "config/database.php";
+<body>
+    <header>
+        <h1>Tilbakemeldinger for emner</h1>
+    </header>
+    <main>
+        <form method="post">
+            <label>Pin-kode:</label>
+            <input type="number" name='pinkode' min="0" max="9999">
+            <button type="submit" name="PinButton">Søk</button>
+        </form>
+        <a href="./logout.php">Logg ut</a>
+        <?php
+        // When guest user comment on message, sudent_id = NULL
+        $currentStudentId = 0;
+        if (session_id() != '') {
+            //session has started
+            if (isset($_SESSION["student_id"])) {
+                $currentStudentId = $_SESSION["student_id"];
+            }
+        }
 
-                // Forms actions:
-                if (isset($_POST["PinButton"])){
-                   getEmneInfo($_POST["pinkode"]);
-                   showMessage($_POST["pinkode"]);
-                }
-            
-                if (isset($_POST["button"])){
-                    $pin = "[pin]";
-                    getEmneInfo($_POST["pin"]);
-                    if ($currentStudentId >1){
-                        //IF Student:
-                        commentMessage($_POST["kommenter"], $_POST["meldingID"], $_POST["studentID"]);
-                    }
-                    else{
-                        //IF guest:
-                        commentMessage($_POST["kommenter"], $_POST["meldingID"], 0);
-                    }
-                    showMessage($_POST["pin"]);
-                }
-                
-                if(isset($_POST['rapporter'])){
-                    $id = "[report]";
-                    $pin = "[pin]";
-                    getEmneInfo($_POST["pin"]);
-                    reportMessage($_POST["report"]);
-                    showMessage($_POST["pin"]);
-                    
-                }
-            ?>
+        echo "<br>";
 
-        </main>
-    </body>
+        include_once "config/database.php";
+
+        // Forms actions:
+        if (isset($_POST["PinButton"])) {
+            getEmneInfo($_POST["pinkode"]);
+            showMessage($_POST["pinkode"]);
+        }
+
+        if (isset($_POST["button"])) {
+            $pin = "[pin]";
+            getEmneInfo($_POST["pin"]);
+            if ($currentStudentId > 1) {
+                //IF Student:
+                commentMessage($_POST["kommenter"], $_POST["meldingID"], $_POST["studentID"]);
+            } else {
+                //IF guest:
+                commentMessage($_POST["kommenter"], $_POST["meldingID"], 0);
+            }
+            showMessage($_POST["pin"]);
+        }
+
+        if (isset($_POST['rapporter'])) {
+            $id = "[report]";
+            $pin = "[pin]";
+            getEmneInfo($_POST["pin"]);
+            reportMessage($_POST["report"]);
+            showMessage($_POST["pin"]);
+        }
+        ?>
+
+    </main>
+</body>
+
 </html>
 
 <?php
 
-function sqlQuery($sql){
+function sqlQuery($sql)
+{
     $database = new Database();
     $db = $database->connect();
     $stmt = $db->query($sql);
     return $stmt;
 }
 
-function fetchArray($sql){
+function fetchArray($sql)
+{
     $stmt = sqlQuery($sql);
     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $row;
 }
 
-function getEmneInfo($pin){
-    $sql ="SELECT emne.emnekode, emne.emnenavn, emne.pinkode 
+function getEmneInfo($pin)
+{
+    $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode 
         FROM emne WHERE pinkode='$pin' limit 1";
     $row = sqlQuery($sql);
 
-    if($row){
-       echo "<h2> Emne info: </h2>"; 
+    if ($row) {
+        echo "<h2> Emne info: </h2>";
         foreach ($row as $row) {
-            echo "<br>Emnekode:". $row["emnekode"]. "<br>Emnenavn: " . $row["emnenavn"]. "<br>Pin-kode: " . $row["pinkode"];
+            echo "<br>Emnekode:" . $row["emnekode"] . "<br>Emnenavn: " . $row["emnenavn"] . "<br>Pin-kode: " . $row["pinkode"];
         }
     }
 }
 
-function getMessage($pin){
+function getMessage($pin)
+{
     $sql = "SELECT emne.emnekode, emne.emnenavn, emne.pinkode, 
          melding.svar, melding.spørsmål, melding.melding_id
          FROM emne INNER JOIN melding ON emne.emne_id = melding.emne_id WHERE pinkode='$pin'";
@@ -105,37 +110,39 @@ function getMessage($pin){
     return $row;
 }
 
-function getComment($meldingId){  
+function getComment($meldingId)
+{
     $sql = "SELECT kommentar.kommentar_id, kommentar.melding_id, kommentar.kommentar FROM kommentar WHERE melding_id='$meldingId'";
     $row = sqlQuery($sql);
     return $row;
 }
 
-function showMessage($pin){
+function showMessage($pin)
+{
     $row = getMessage($pin);
     global $currentStudentId;
-    if($row){
+    if ($row) {
         //Spørsmål and svar:
         foreach ($row as $row) {
-            echo "<br><br> Spørsmål: ". $row["spørsmål"]; 
-                if (!empty($row["svar"])){ 
-                    echo "<br> Svar: ". $row["svar"];
-                }
+            echo "<br><br> Spørsmål: " . $row["spørsmål"];
+            if (!empty($row["svar"])) {
+                echo "<br> Svar: " . $row["svar"];
+            }
 
             //Kommentar:
             $comment = getComment($row["melding_id"]);
             if ($comment);
-                foreach ($comment as $comment){
-                    echo "<br> Kommentar: ";
-                    echo $comment["kommentar"];  
+            foreach ($comment as $comment) {
+                echo "<br> Kommentar: ";
+                echo $comment["kommentar"];
             }
 
-            ?>
+?>
             <!-- Send kommentar -->
             <form method="POST">
-                <input type="hidden" name="studentID" value="<?php echo $currentStudentId;?>">    
-                <input type="hidden"  name='meldingID' value="<?php echo $row['melding_id']?>">
-                <input type="hidden" name="pin" value="<?php echo $pin?>">
+                <input type="hidden" name="studentID" value="<?php echo $currentStudentId; ?>">
+                <input type="hidden" name='meldingID' value="<?php echo $row['melding_id'] ?>">
+                <input type="hidden" name="pin" value="<?php echo $pin ?>">
                 <label>Kommentar:</label>
                 <textarea name="kommenter"></textarea>
                 <input type="submit" name="button" value="Svar">
@@ -143,33 +150,34 @@ function showMessage($pin){
 
             <!-- Rapporter melding -->
             <form method="POST">
-                
-                <input type="hidden" name='report' value="<?php echo $row['melding_id']?>">
-                <input type="hidden" name="pin" value="<?php echo $pin?>">
+
+                <input type="hidden" name='report' value="<?php echo $row['melding_id'] ?>">
+                <input type="hidden" name="pin" value="<?php echo $pin ?>">
                 <input id="report" type="submit" name="rapporter" value="Rapporter">
             </form>
-            <?php
-              
+<?php
+
         }
     }
 }
 
-function commentMessage($kommentar, $melding_id, $currentStudentId){
-    if ($currentStudentId > 1){
+function commentMessage($kommentar, $melding_id, $currentStudentId)
+{
+    if ($currentStudentId > 1) {
         // IF students:
         $sql = "INSERT INTO kommentar (kommentar, melding_id, student_id) 
                 VALUES ('$kommentar', '$melding_id', '$currentStudentId')";
         sqlQuery($sql);
-    }
-    else{
+    } else {
         // If guest users:
         $sql = "INSERT INTO kommentar (kommentar, melding_id, student_id) 
                 VALUES ('$kommentar', '$melding_id', NULL)";
         sqlQuery($sql);
-    } 
+    }
 }
 
-function reportMessage($id){
+function reportMessage($id)
+{
     $sql = "UPDATE melding SET upassende_melding = COALESCE(upassende_melding)+1 WHERE melding_id = $id";
     sqlQuery($sql);
 }
