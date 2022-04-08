78ee71853a8789548d8ce8028c369bcfe39b14ba
Maiken Persen <54583559+maikenfp@users.noreply.github.com>
2022-04-08 21:26:54 +0200

    Oppdatert DB config

--- config/Database.php
+++ config/Database.php
@@ -2,11 +2,21 @@
 class Database
 {
   // DB Params
-  private $host = '158.39.188.205';
-  private $db_name = 'datasikkerhet';
-  private $username = 'datasikkerhet';
-  private $password = '1234';
+  private $host = 'localhost';
+  private $db_name = 'datasikkerhet2fqwldersd';
+  private $username = 'adminr4gtQwerT32';
+  private $password = 'Fle3Cwdsefdeg4';
   private $conn;
+
+  private $host2 = '158.39.188.205';
+
+  private $usernameStudent = 'student';
+  private $usernameForeleser = 'foreleser';
+  private $usernameGjest = 'gjest';
+
+  private $passwordStudent = 'WtFdwe42t43fde';
+  private $passwordForeleser = 'Fve4gfw43ftwy';
+  private $passwordGjest = '23btxdbtw4gbe5';
 
   // DB Connect
   public function connect()
@@ -20,7 +30,53 @@ class Database
     } catch (PDOException $e) {
       echo 'Connection Error: ' . $e->getMessage();
     }
+    
+    return $this->conn;
+  }
+
+  public function connectStudent()
+  {
+    $this->conn = null;
+
+    try {
+      $this->conn = new PDO('mysql:host=' . $this->host2 . ';dbname=' . $this->db_name, $this->usernameStudent, $this->passwordStudent);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->conn->exec("set names utf8");
+    } catch (PDOException $e) {
+      echo 'Connection Error: ' . $e->getMessage();
+    }
 
     return $this->conn;
   }
+
+  public function connectForeleser()
+  {
+    $this->conn = null;
+
+    try {
+      $this->conn = new PDO('mysql:host=' . $this->host2 . ';dbname=' . $this->db_name, $this->usernameForeleser, $this->passwordForeleser);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->conn->exec("set names utf8");
+    } catch (PDOException $e) {
+      echo 'Connection Error: ' . $e->getMessage();
+    }
+
+    return $this->conn;
+  }
+
+  public function connectGjest()
+  {
+    $this->conn = null;
+
+    try {
+      $this->conn = new PDO('mysql:host=' . $this->host2 . ';dbname=' . $this->db_name, $this->usernameGjest, $this->passwordGjest);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->conn->exec("set names utf8");
+    } catch (PDOException $e) {
+      echo 'Connection Error: ' . $e->getMessage();
+    }
+
+    return $this->conn;
+  }
+
 }
