562e75c47070b26c062eb6957f17b670a0aafe18
Børre Stellander <64796748+stellanova88@users.noreply.github.com>
2022-04-11 13:04:36 +0200

    Update guest.php

--- guest.php
+++ guest.php
@@ -147,8 +147,8 @@ function getForeleserBilde($pin){
 
 function sqlQuery($sql){
     $database = new Database();
-    // $db = $database->connect();
-    $db = $database->connectGjest();
+    $db = $database->connect();
+    // $db = $database->connectGjest();
     $stmt = $db->query($sql);
     return $stmt;
 }
